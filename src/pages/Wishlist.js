b06a7067f00c10691e49987700dff9db67fa3c9a
Krrish-29 <kingkrrish29@gmail.com>
2026-02-06 16:25:31 +0530

    Frontend

--- src/pages/Wishlist.js
+++ src/pages/Wishlist.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import  useEffect  from "react";
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
 import Container from "../components/Container";
